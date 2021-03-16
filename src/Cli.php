64c88ad9ef8c450fc644d1e8db82f960cbaef6a3
vasily.smolin <vasilysmoolin@gmail.com>
2021-03-16 18:12:24 +0400

    спрашиваем имя в консоле

---
+++ src/Cli.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Brain;
+
+use function cli\line;
+use function cli\prompt;
+
+class Cli
+{
+
+    public $userName;
+
+    public function start()
+    {
+        line('Welcome to the Brain Games!');
+        $this->userName = prompt('May I have your name?');
+        line("Hello, %s!", $this->userName);
+
+    }
+
+}

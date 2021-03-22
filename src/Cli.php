8ce33d7efa3ce14f2c976abe4b927434c4c25949
vasily.smolin <vasilysmoolin@gmail.com>
2021-03-22 16:22:26 +0400

    mini test

--- src/Cli.php
+++ src/Cli.php
@@ -11,7 +11,7 @@ class Cli
 
     public function start()
     {
-        line('Welcome to the Brain Games!');
+        line('Welcome to the Brain Games! ');
         $this->userName = prompt('May I have your name?');
         line("Hello, %s!", $this->userName);
     }
